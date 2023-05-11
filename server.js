062f3708d52e70b63e2a154881483aebc95509d6
prateekvarma <prtk.varma@gmail.com>
2023-05-11 14:13:59 -0600

    Added local SSL using OPENSSL and condfigured app

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
+const fs = require('fs');
 const path = require('path');
+const https = require('https');
 const express = require('express');
 
 const PORT = 3000;
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', index.html));
+app.get('/secret', (req, res) => {
+  return res.send('Your personal secret value');
 });
 
-app.listen(PORT, () => {
-  console.log(`Listening on PORT ${PORT}...`);
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
+https
+  .createServer({
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem'),
+  }, app)
+  .listen(PORT, () => {
+    console.log(`Listening on PORT ${PORT}...`);
+  });
