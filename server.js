d426a0ba61c95631a6d59b62b0dfeee6f4da585c
prateekvarma <prtk.varma@gmail.com>
2023-05-17 16:48:20 -0600

    Using passport to SSO login with google

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const path = require('path');
 const https = require('https');
 const express = require('express');
 const helmet = require('helmet');
+const passport = require('passport');
+const { Strategy } = require('passport-google-oauth20');
 
 require('dotenv').config();
 
@@ -11,11 +13,24 @@ const config = {
   CLIENT_SECRET: process.env.CLIENT_SECRET,
 };
 
+const AUTH_OPTIONS = {
+  callbackURL: '/auth/google/callback',
+  clientID: config.CLIENT_ID,
+  clientSecret: config.CLIENT_SECRET,
+};
+
+function verifyCallback(accessToken, refreshToken, profile, done) {
+  console.log('Google profile: ', profile);
+}
+
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
+
 const PORT = 3000;
 
 const app = express();
 
 app.use(helmet());
+app.use(passport.initialize());
 
 function checkLoggedIn(req, res, next) {
   const isLoggedIn = true;
@@ -27,12 +42,27 @@ function checkLoggedIn(req, res, next) {
   next();
 }
 
-app.get('/auth/google', (req, res) => {});
-app.get('/auth/google/callback', (req, res) => {});
+app.get('/auth/google', passport.authenticate('google', { scope: ['email'] }));
+
+app.get(
+  '/auth/google/callback',
+  passport.authenticate('google', {
+    failureRedirect: '/failure',
+    successRedirect: '/',
+    session: false,
+  }),
+  (req, res) => {
+    console.log('Callback from google');
+  }
+);
 app.get('/auth/logout', (req, res) => {});
 
 app.get('/secret', checkLoggedIn, (req, res) => {
   return res.send('Your personal secret value');
+});
+
+app.get('/failure', (req, res) => {
+  return res.send('Failed to login!');
 });
 
 app.get('/', (req, res) => {
